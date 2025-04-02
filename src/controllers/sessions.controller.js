7bff0961feca185eaed2bcd3b0478823a8f67b2f
emipere <emipereiro@gmail.com>
2025-04-02 11:45:39 -0300

    limpie codigo comentado

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -10,27 +10,6 @@ router.get( "/github",
   async (req, res) => {}
 );
 
-// router.get("/githubcallback",
-//   passport.authenticate("github", { failureRedirect: "/github/error" }),
-//   async (req, res) => {
-//     const user = req.user;
-//     const tokenUser = {
-//       name: `${user.first_name} ${user.last_name}`,
-//       email: user.email,
-//       age: user.age,
-//       cart: user.cart,
-//       role: user.role,
-//     };
-//     const access_token = generateJWToken(tokenUser);
-
-//     res.cookie("jwtCookieToken", access_token, {
-//       maxAge: 60000,
-//       httpOnly: true,
-//     });
-
-//     res.redirect("/users");
-//   }
-// );
 export const githubCallback =   
 async (req, res) => {
   try {
@@ -54,17 +33,7 @@ async (req, res) => {
     res.status(500).send({ status: "error", error: "Error en GitHub callback" });
   }
 };
-// router.post("/register",
-//   passport.authenticate("register", {
-//     failureRedirect: "/api/sessions/fail-register",
-//   }),
-//   async (req, res) => {
-//     // console.log("Registrando nuevo usuario.");
-//     res
-//       .status(201)
-//       .send({ status: "success", message: "Usuario creado con extito." });
-//   }
-// );
+
 export const registerUser =  
 async (req, res) => {
   try {
@@ -75,52 +44,11 @@ async (req, res) => {
     res.status(500).send({ status: "error", error: "Error al registrar usuario" });
   }
 };
-// router.get("/fail-register", (req, res) => {
-//   res.status(401).send({ error: "Failed to process register!" });
-// });
+
 export const failRegister = (req, res) => {
   res.status(401).send({ error: "Failed to process register!" });
 };
 
-// router.post("/login", async (req, res) => {
-//   const { email, password } = req.body;
-
-//   try {
-//     const user = await userModel.findOne({ email: email });
-//     if (!user)
-//       return res.status(401).json({ message: "Usuario no encontrado" });
-
-//     if (!isValidPassword(user, password)) {
-//       // console.warn("Invalid credentials for user: " + email);
-//       return res
-//         .status(401)
-//         .send({ status: "error", error: "Credenciales invalidas!!!" });
-//     }
-
-//     const tokenUser = {
-//       name: `${user.first_name} ${user.last_name}`,
-//       email: user.email,
-//       age: user.age,
-//       role: user.role,
-//       isAdmin: user.role === "admin",
-//     };
-
-//     const access_token = generateJWToken(tokenUser);
-//     // console.log("access_token", access_token);
-
-//     res.cookie("jwtCookieToken", access_token, {
-//       maxAge: 60000,
-//       httpOnly: true,
-//     });
-
-//     res.send({ message: "Login successfull" });
-//   } catch (error) {
-//     // console.error(error);
-//     return res
-//       .status(500)
-//       .send({ status: "error", error: "Error interno de la app." });
-//   }
-// });
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
@@ -158,19 +86,12 @@ export const loginUser = async (req, res) => {
       .send({ status: "error", error: "Error interno de la app." });
   }
 };
-// router.get("/logout", (req, res) => {
-//   res.clearCookie("jwtCookieToken");
-//   res.redirect("/users/login");
-// });
+
 export const logoutUser = (req, res) => {
   res.clearCookie("jwtCookieToken");
   res.redirect("/users/login");
 };
-// router.get("/current",
-//   passport.authenticate("current", { session: false }),
-//   (req, res) => {
-//     res.send(req.user);
-//   }
+
 
 export const getCurrentUser =  
 (req, res) => {
